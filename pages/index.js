f07313c7558d05580773d55398faf91c42094fa3
Bartłomiej Głowala <bartlomiejglowala@gmail.com>
2021-12-16 00:01:47 +0100

    Add example data fetch

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className="container">
       <Head>
@@ -14,10 +14,29 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+          <div style={{display: 'none'}}>
+          {JSON.stringify(props.data, null, 2)}
+          </div>
         </p>
       </main>
 
       <Footer />
     </div>
   )
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(`https://backoffice.zaproszenia.com/wp-json/wc/v3/products`, {
+    headers: {
+      'Authorization': 'Basic Y2tfMWUzZDVhNDMyZTI2NTAyODE2ODlhOWI0ZjQwZGQ1ZGJmMzliMTE0ZDpjc184M2JkZDIxMWQ2MmFmNDg1NzE4NGI3YjY3YzM1ZGU4M2I1MDUxYjgw',
+    }
+  })
+  const data = await res.json()
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60,
+  };
 }
